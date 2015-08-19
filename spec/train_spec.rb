890e6972e3f99f5c68b258fb02ebb75b4545a0ca
Christoph PIrringer & Jason Lane <student@epicodus.com>
2015-08-19 09:43:55 -0700

    Initial commit

---
+++ spec/train_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Train do
+
+  before do
+    @train = Train.new({ id: nil, name: "City_Of_New_Orleans"})
+  end
+
+  describe '#id' do
+    it 'returns trains id initially set to 0' do
+      expect(@train.id).to eq 0
+    end
+  end
+
+  describe '#name' do
+    it 'returns trains name' do
+      expect(@train.name).to eq "City_Of_New_Orleans"
+    end
+  end
+
+
+end
